c7fc2c527b6d852e4ca165889460d2988e27c41d
OFFICE\Feiui.jie <feiui.jie@cardinfolink.com>
2022-10-14 17:54:43 +0800

    faet: demo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+      <v-formly ref="form" v-model="data" :meta="meta"> </v-formly>
+      <div class="btns">
+        <a-button type="danger" @click="clear"> 重置 </a-button>
+        <a-button type="primary" @click="submit"> 提交 </a-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function () {
+    return {
+      meta: {
+        type: "object",
+        properties: {
+          name: {
+            title: "姓名",
+            type: "string",
+            default: "kevin",
+            ui: {
+              showRequired: true,
+            },
+          },
+          desc: {
+            title: "描述",
+            type: "string",
+            default: "Base on technical, but not limited on it!",
+            ui: {
+              change: (val) => {
+                console.log("val = ", val);
+              },
+            },
+          },
+          enable: {
+            title: "启用",
+            type: "boolean",
+          },
+        },
+        required: ["name"],
+      },
+      data: { enable: true },
+    };
+  },
+  methods: {
+    clear() {
+      this.data = null;
+    },
+    async submit() {
+      let valid = await this.$refs.form.validate();
+      if (valid) {
+        this.$message.success(JSON.stringify(this.data));
+      }
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,8 +65,18 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.wrapper {
+  margin: auto;
+  max-width: 600px;
+}
+.btns {
+  display: flex;
+  justify-content: end;
+}
+.ant-btn + .ant-btn {
+  margin-left: 8px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+import VFormly from "v-formly";
 
 Vue.config.productionTip = false
+
+Vue.use(Antd);
+Vue.use(VFormly);
+
 
 new Vue({
   render: h => h(App),
